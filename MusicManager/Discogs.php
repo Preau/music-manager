7734a6488cef75f91dd9865581b0a6191cb5f868
Koen van Zuijlen <khavanzuijlen@gmail.com>
2016-08-25 15:39:36 +0200

    Add basic Discogs API class

---
+++ MusicManager/Discogs.php
@@ -0,0 +1,36 @@
+<?php
+namespace MusicManager;
+
+class Discogs {
+	const BASE_URL = 'https://api.discogs.com/';
+
+	/**
+	 * Do a request to Discogs API
+	 * @param string $url
+	 * @return array
+	 */
+	private function request($url) {
+		$ch = curl_init(self::BASE_URL . $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+			'User-Agent: music-manager/1.0.0',
+			'Authorization: Discogs token=' . getenv('DISCOGS_TOKEN')
+		]);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+
+		//Check if an error occured
+		if(curl_errno($ch) !== 0) {
+			return ['error' => curl_errno($ch) . ': ' . curl_error($ch)];
+		}
+
+		return json_decode($response, JSON_OBJECT_AS_ARRAY);
+	}
+
+	/**
+	 * Get the data for a master release
+	 * @param int $identifier
+	 */
+	public function getMaster($identifier) {
+		print_r(self::request('masters/'.$identifier));
+	}
+}
