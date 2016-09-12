395e11fc6a32bc92bfc8dde799998341da796f93
Koen van Zuijlen <khavanzuijlen@gmail.com>
2016-09-12 23:09:42 +0200

    Move all library handling to Library class, add comparison

--- MusicManager/Library.php
+++ MusicManager/Library.php
@@ -5,6 +5,22 @@ class Library {
 	const FILEPATH = __DIR__.'/../library.json';
 
 	/**
+	 * Load the library for usage
+	 * @return array
+	 */
+	public static function loadLibrary() {
+		$dirs = self::readDirectories();
+		$library = self::readFile();
+
+		if($library == null) {
+			return self::initFile($dirs);
+		}
+
+		return self::compareLibraryAndDirectories($library, $dirs);
+	}
+
+	/**
+	 * Read the library.json file if it exists
 	 * @return array|null
 	 */
 	public static function readFile() {
@@ -15,18 +31,72 @@ class Library {
 		}
 	}
 
-	public static function writeFile($data) {
+	/**
+	 * Write data to the library.json file
+	 * @param array $data
+	 */
+	private static function writeFile($data) {
 		file_put_contents(self::FILEPATH, json_encode($data));
 	}
 
 	/**
+	 * Initialise the library.json file
 	 * @param array $dirs
+	 * @return array
 	 */
-	public static function initFile($dirs) {
+	private static function initFile($dirs) {
 		$library = [];
 		foreach($dirs as $dir) {
 			$library[$dir] = [];
 		}
 		self::writeFile($library);
+		return $library;
+	}
+
+	/**
+	 * Read and return a list of directories from the music library
+	 * @return array
+	 */
+	private static function readDirectories() {
+		$path = getenv('MUSIC_LIBRARY');
+		$handle = opendir($path);
+		$dirs = [];
+		while($entry = readdir($handle)) {
+			if(is_dir($path.DIRECTORY_SEPARATOR.$entry) && substr($entry, 0, 1) !== '.') {
+				$dirs[] = utf8_encode($entry);
+			}
+		}
+		natcasesort($dirs);
+		return array_values($dirs);
+	}
+
+	/**
+	 * Compare the current library file with the library and change it accordingly
+	 * @param array $library
+	 * @param array $dirs
+	 * @return array
+	 */
+	private static function compareLibraryAndDirectories($library, $dirs) {
+		$libraryClone = $library;
+
+		foreach($dirs as $dir) {
+			if(!isset($library[$dir])) {
+				//Doesn't exist in file add it
+				$library[$dir] = [];
+			} else {
+				//Already exists in file, remove it from clone, remove possible deleted flag
+				unset($libraryClone[$dir]);
+				unset($library[$dir]['deleted']);
+			}
+		}
+
+		//Everything remained in clone is deleted from library, add deleted flag
+		foreach(array_keys($libraryClone) as $dir) {
+			$library[$dir]['deleted'] = 1;
+		}
+
+		array_multisort(array_keys($library), SORT_NATURAL|SORT_FLAG_CASE, $library);
+		self::writeFile($library);
+		return $library;
 	}
 }
--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
+use MusicManager\Library;
+
 require 'partials/head.php';
 
-//Read directories
-$path = getenv('MUSIC_LIBRARY');
-$handle = opendir($path);
-$dirs = [];
-while($entry = readdir($handle)) {
-	if(is_dir($path.DIRECTORY_SEPARATOR.$entry) && substr($entry, 0, 1) !== '.') {
-		$dirs[] = utf8_encode($entry);
-	}
-}
-natcasesort($dirs);
-$dirs = array_values($dirs);
-
-//Read library file
-$library = \MusicManager\Library::readFile();
-if($library == null) {
-	//Create the library
-	\MusicManager\Library::initFile($dirs);
-}
+//Load the library
+$library = Library::loadLibrary();
 
 ?>
 <html>
@@ -28,14 +14,16 @@ if($library == null) {
 	<body>
 		<form method="post" action="">
 			<button type="submit">Save everything</button>
-			<? for($i = 0; $i < sizeof($dirs); $i++) {
-				$dir = $dirs[$i]; ?>
+			<? $i = 1;
+			foreach($library as $dir => $data) { ?>
 				<div class="directory">
 					<?=$dir?>
 					<input type="hidden" name="dir[<?=$i?>]" value="<?=$dir?>">
 					<input type="text" name="master[<?=$i?>]" value="">
+					<a href="https://www.discogs.com/search?q=<?=urlencode($dir)?>&type=master" target="_blank">Search on Discogs</a>
 				</div>
-			<? } ?>
+				<? $i++;
+			} ?>
 		</form>
 	</body>
 </html>
