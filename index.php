6aa93c040f57a2c33e610672a4cbcf222001acaf
Koen van Zuijlen <khavanzuijlen@gmail.com>
2016-10-10 20:34:14 +0200

    Add twig template for index

--- index.php
+++ index.php
@@ -3,39 +3,16 @@ use MusicManager\Library;
 
 require 'partials/head.php';
 
+//Init twig
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new Twig_Environment($loader, array(
+	'cache' => __DIR__.'/_cache',
+));
+
 //Load the library
 $library = Library::loadLibrary();
 
+echo $twig->render('index.twig', [
+	'library' => $library
+]);
 ?>
-<html>
-	<head>
-		<title>Music Manager</title>
-		<script src="build/libraries.js" type="text/javascript"></script>
-		<script src="build/script.js" type="text/javascript"></script>
-	</head>
-	<body>
-		<form method="post" action="post.php">
-			<button type="submit" name="request" value="savemasterids">Save everything</button>
-			<? foreach($library as $number => $data) {
-				if(!isset($data['deleted'])) { ?>
-					<div class="directory">
-						<?=$data['dir']?>
-						<br>
-						<input type="text" id="master-<?=$number?>" name="master[<?=$number?>]" value="<?=(isset($data['master']) ? $data['master'] : '')?>">
-						<br>
-						File genre: <span id="genre-file-<?=$number?>"></span>
-						<br>
-						Discogs genre: <span id="genre-discogs-<?=$number?>"></span>
-						<br>
-						<button type="button" onclick="checkGenres(<?=$number?>)">Find genre</button>
-						<button type="button" id="save-genre-<?=$number?>" onclick="saveGenre(<?=$number?>)" disabled="disabled">Save genre</button>
-						<br>
-						<a href="https://www.discogs.com/search?q=<?=urlencode($data['dir'])?>&type=master" target="_blank">Search on Discogs</a>
-
-						<br><br><br>
-					</div>
-				<? }
-			} ?>
-		</form>
-	</body>
-</html>
